4426fcdbd0af682416bb4d5e0420607b6e5484ac
Ubuntu <ubuntu@ip-172-31-4-16.ec2.internal>
2022-02-25 04:48:45 +0000

    modified script.sh

--- script.sh
+++ script.sh
@@ -13,5 +13,5 @@ usermod -a -G docker ubuntu
 chmod 666 /var/run/docker.sock
 chkconfig docker on
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 931058976119.dkr.ecr.us-east-1.amazonaws.com
-docker pull 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
-docker run -d -it -p 3000:3000 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp
+docker pull 931058976119.dkr.ecr.us-east-1.amazonaws.com/jenkins-pipeline-build-demo:latest
+docker run -d -it -p 3000:3000 931058976119.dkr.ecr.us-east-1.amazonaws.com/jenkins-pipeline-build-demo:latest
