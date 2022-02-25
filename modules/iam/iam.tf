c9d680062d14606a6464fcc82dfb74dcf74cf619
Ubuntu <ubuntu@ip-172-31-4-16.ec2.internal>
2022-02-25 09:19:05 +0000

    modified

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "kapil_ec2_role" {
   assume_role_policy = file("ec2-assume-policy.json")
 
 }
-/*
+
 resource "aws_iam_instance_profile" "ec2-profile" {
   name = "kapil_ec2_profile"
   role = aws_iam_role.kapil_ec2_role.name
